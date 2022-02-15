ccfa831ca26ce312f60156fa6013173636f0a2ce
Nick Rice <enprice383@gmail.com>
2022-02-15 18:13:20 +0000

    Initial Commit

---
+++ DesignCodeiOS15/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeiOS15
+//
+//  Created by Nick Rice on 15/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Nick Rice on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
