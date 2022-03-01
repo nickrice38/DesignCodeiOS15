3ae2d72f5012109da7aedc34e7572bb90802cd09
Nick Rice <enprice383@gmail.com>
2022-03-01 19:04:43 +0000

    - Horizontal list of FeaturedItems - Started building out matchedGeometryEffect views

---
+++ DesignCodeiOS15/Components/FeaturedItem.swift
@@ -0,0 +1,61 @@
+//
+//  FeaturedItem.swift
+//  DesignCodeiOS15
+//
+//  Created by Nick Rice on 24/02/2022.
+//
+
+import SwiftUI
+
+struct FeaturedItem: View {
+    var course: Course = courses[0]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            
+            Image(course.logo)
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(10.0)
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+                .strokeStyle(cornerRadius: 16)
+            
+            Text(course.title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .lineLimit(1)
+            
+            Text(course.subtitle.uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            
+            Text(course.text)
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+            
+        }
+        .padding(.all, 20.0)
+        .padding(.vertical, 20)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial)
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+//        .cornerRadius(30.0)
+//        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .strokeStyle()
+        .padding(.horizontal, 20)
+    }
+}
+
+struct FeaturedItem_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedItem()
+    }
+}
--- DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/Views/HomeView.swift
@@ -11,18 +11,32 @@ struct HomeView: View {
     @State var hasScrolled = false
     
     var body: some View {
-        ScrollView {
-            GeometryReader { proxy in
-//                Text("\(proxy.frame(in: .named("scroll")).minY)")
-                Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            
+            ScrollView {
+                scrollDetection
+                
+                featured
+                
+                Color.clear.frame(height: 1000)
             }
-            .frame(height: 0)
-            
-            FeaturedItem()
-            
-            Color.clear.frame(height: 1000)
+            .coordinateSpace(name: "scroll")
+            .safeAreaInset(edge: .top, content: {
+                Color.clear.frame(height: 70)
+            })
+            .overlay(
+                NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
+        )
         }
-        .coordinateSpace(name: "scroll")
+    }
+    
+    var scrollDetection: some View {
+        GeometryReader { proxy in
+//                Text("\(proxy.frame(in: .named("scroll")).minY)")
+            Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+        }
+        .frame(height: 0)
         .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
             withAnimation(.easeInOut) {
                 if value < 0 {
@@ -32,11 +46,37 @@ struct HomeView: View {
                 }
             }
         })
-        .safeAreaInset(edge: .top, content: {
-            Color.clear.frame(height: 70)
-        })
-        .overlay(
-            NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
+    }
+    
+    var featured: some View {
+        TabView {
+            ForEach(courses) { course in
+                GeometryReader { proxy in
+                    let minX = proxy.frame(in: .global).minX
+                    
+                    FeaturedItem(course: course)
+                        .padding(.vertical, 40)
+                        .rotation3DEffect(.degrees(minX / -10), axis: (x: 0, y: 1, z: 0))
+                        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+                        .blur(radius: abs(minX / 40))
+                        .overlay(
+                            Image(course.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 230)
+                                .offset(x: 32, y: -80)
+                                .offset(x: minX / 2)
+                        )
+                    
+//                    Text("\(proxy.frame(in: .global).minX)")
+                }
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(height: 430)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
         )
     }
 }
